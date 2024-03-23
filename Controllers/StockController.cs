3a761b82a98ef3cc4e32d899719556b4210d2001
HatemKhabir <hatemkhabir.cse@gmail.com>
2024-03-23 11:43:57 +0100

    crud operations and entity framework usage

---
+++ Controllers/StockController.cs
@@ -0,0 +1,77 @@
+﻿using api.Data;
+using api.Dtos.Stock;
+using api.Mapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+	[Route("api/stocks")]
+	[ApiController]
+	public class StockController : ControllerBase
+	{
+		private readonly ApplicationDBContext _context;
+		public StockController(ApplicationDBContext context) {
+		this._context = context;
+
+		}
+
+		[HttpGet]
+		public IActionResult GetAll() {
+			var stocks = _context.Stocks.ToList().Select(s=>s.ToStockDto());
+			if (stocks == null)
+			{
+				return NotFound();
+			}
+			return Ok(stocks);
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetById([FromRoute]int id)
+		{var stock= _context.Stocks.FirstOrDefault(s => s.Id == id);
+			if (stock == null)
+			{
+				return NotFound();
+			}
+			return Ok(stock);
+
+		}
+		[HttpPost]
+		public IActionResult addStock([FromBody] PostRequestDTO stockDto)
+		{
+			var stockModel=stockDto.ToStockFromPostDto();
+			_context.Stocks.Add(stockModel);
+			_context.SaveChanges();
+			return CreatedAtAction(nameof(GetById), new {id=stockModel.Id},stockModel.ToStockDto());
+
+		}
+		[HttpPut]
+		[Route("{id}")]
+		public IActionResult Update([FromRoute]int id, [FromBody] PutRequestDto stockModel)
+		{
+			var findStock=_context.Stocks.FirstOrDefault(s=>s.Id == id);
+			if (findStock == null)
+			{ return NotFound(); }
+			findStock.Symbol=stockModel.Symbol;
+			findStock.CompanyName=stockModel.CompanyName;
+			findStock.Industry=stockModel.Industry;
+			findStock.Purchase=stockModel.Purchase;
+			findStock.MarketCap=stockModel.MarketCap;
+
+			_context.SaveChanges();
+			return Ok(findStock.ToStockDto());
+
+		}
+		[HttpDelete]
+		[Route("{id}")]
+		public IActionResult deleteStock([FromRoute]int id)
+		{
+			var stock = _context.Stocks.FirstOrDefault(s => s.Id == id);
+			if (stock==null)
+			{ return NotFound(); }
+			_context.Stocks.Remove(stock);
+			_context.SaveChanges();
+			return NoContent();
+		}
+
+	}
+}
---
+++ Dtos/Stock/StockDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Dtos.Socket
+{
+	//what object returned will give back with the mapper as well , for example stock table have comments but i will return everything except comments
+	public class StockDto
+	{
+		public int Id { get; set; }
+		public string Symbol { get; set; } = string.Empty;
+		public string CompanyName { get; set; } = string.Empty;
+
+		public decimal Purchase { get; set; }
+		public decimal LastDiv { get; set; }
+		public string Industry { get; set; } = string.Empty;
+		public long MarketCap { get; set; }
+	}
+}
